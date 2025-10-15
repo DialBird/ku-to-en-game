af9074f9494725793be49a31baeb58c3c166abfa
Keisuke Taniguchi <rememberthatk@icloud.com>
2025-10-15 18:57:43 +0900

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
       <p className="text-red-400">Hello World</p>
+      <Button>Click me</Button>
     </div>
   );
 }
